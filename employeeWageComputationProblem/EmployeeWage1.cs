f59deae2b49ef3d3b7b2f5e0c9fd3ac01de82d79
“Akhilkumar1f4” <akhilkumar1f4@gmail.com>
2023-05-23 21:26:24 +0530

    UC1:Employeettendanceadded

---
+++ employeeWageComputationProblem/EmployeeWage1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeeWageComputationProblem
+{
+    internal class EmployeeWage1
+    {
+        public int WagePerHour { get; private set; }
+        public int FullDayHours { get; private set; }
+        public int PartTimeHours { get; private set; }
+
+        public EmployeeWage1(int wagePerHour, int fullDayHours, int partTimeHours)
+        {
+            WagePerHour = wagePerHour;
+            FullDayHours = fullDayHours;
+            PartTimeHours = partTimeHours;
+        }
+
+        public int CalculateDailyWage(bool isFullTime)
+        {
+            int dailyWage = 0;
+
+            if (isFullTime)
+                dailyWage = WagePerHour * FullDayHours;
+            else
+                dailyWage = WagePerHour * PartTimeHours;
+
+            return dailyWage;
+        }
+    }
+}
+
+    
